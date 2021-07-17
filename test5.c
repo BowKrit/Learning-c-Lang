c0d16576a3c374a4bb2802d2854e65bb767327d4
Krittiya <krittiya_b@outlook.com>
2021-07-17 21:45:29 +0700

    test5
    +and* number

---
+++ test5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int summary(int num)
+{
+    int i, sum = 0;
+    for (i = 1; i <= num; ++i)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int multiple(int num)
+{
+    int i, sum = 1;
+    for (i = 1; i <= num; ++i)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+int main()
+{
+
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Sum = %d\n", summary(num));
+    printf("Sum = %d", multiple(num));
+
+    return 0;
+}
